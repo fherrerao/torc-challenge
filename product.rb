cae76992bca4183947d1491b8ad18b78d86180d4
fherrerao <fherrera0206@gmail.com>
2022-10-26 11:06:49 -0500

    Created Product class as pattern and, Imported, Exempt classes as childs

---
+++ product.rb
@@ -0,0 +1,37 @@
+class Product
+  attr_accessor :name, :price, :quantity, :imported, :exempt
+
+  def initialize(name, price, quantity, tax, total=0)
+    @name = name
+    @price = price
+    @quantity = quantity
+    @tax = tax
+    @imported = imported
+    @exempt = exempt
+    @total = total
+  end
+
+  def product_taxes
+    total = quantity*price*(@tax-1)
+    total = total.round(2)
+  end
+
+  def total_product
+    total = quantity*price*@tax
+    total = total.round(2)
+  end  
+end
+
+class Imported < Product
+  def initialize(name, price, quantity, tax)
+    super(name, price, quantity, tax)
+    @tax = tax
+  end
+end
+
+class Exempt < Product
+  def initialize(name, price, quantity, tax)
+    super(name, price, quantity, tax)
+    @tax = tax
+  end
+end
